6c083b5ad574b8243b527ddd685f2e622ec7d741
dawfedora <dawfedora@users.noreply.github.com>
2026-06-01 15:13:59 +0000

    deploy: 53a289fe0465f2f38a678520f9ea270539326523

--- dev/sw.js
+++ dev/sw.js
@@ -1,5 +1,5 @@
 // DO NOT REFORMAT deploy.bash depends on this line
-const CACHE_NAME = 'edgewood-dev-2026.06.01.0809';
+const CACHE_NAME = 'edgewood-dev-2026.06.01.0813';
 
 const APP_SHELL = [
   './',
